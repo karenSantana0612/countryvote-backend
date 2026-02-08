81ab0d9c05b580a62b9055f7c013f4d866615c70
Karen Santana <karendsantana0612@gmail.com>
2026-02-07 23:31:26 -0500

    chore: init spring boot backend white maven and java 17

---
+++ countryvote-backend/src/main/java/com/countryvote/backend/CountryvoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.countryvote.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryvoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryvoteBackendApplication.class, args);
+    }
+
+}
---
+++ countryvote-backend/src/test/java/com/countryvote/backend/CountryvoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countryvote.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryvoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
